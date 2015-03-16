5dd7ee2a72e72c76cc65f4a0e848548eca706c8f
Lily Seropian <lilyseropian@gmail.com>
2015-03-16 10:13:10 -0400

    Added docs and improved row tests

--- classes/row.py
+++ classes/row.py
@@ -1,28 +1,50 @@
 from annotation import *
 
 class Row:
-    def __init__(self, components, number):
+
+    def __init__(self, components, number, ref=None):
+        """Create a new row.
+
+        Keyword arguments:
+        components -- a list of annotations that comprise the row
+        number -- the row number (an integer)
+        ref -- a row number the current row references (default None)
+        """
+
         if type(components) is not list:
             raise Exception('Components must be a list.')
         if len(components) == 0:
             raise Exception('Components must not be empty.')
         if type(number) is not int:
-            raise Exception('Number must be an integer.')
+            raise Exception('Row numbers must be integers.')
+        if ref and type(ref) is not int:
+            raise Exception('Row references must be integers.')
         for component in components:
             if component.__class__ is not Annotation:
                 raise Exception('Each component of a Row must be an Annotation.')
 
         self.components = components
         self.number = number
+        self.ref = ref
 
     def __str__(self):
-        return '<row number="' + str(self.number) + '">' + '\n'.join([component.__str__() for component in self.components]) + '</row>'
+        """Return an XML representation of the row."""
+
+        row_str = '<row number="' + str(self.number)
+        if self.ref:
+            row_str += '" ref="' + self.ref
+        row_str += '">' + '\n'.join([component.__str__() for component in self.components]) + '</row>'
+        return row_str
 
     def __eq__(self, other):
+        """Return whether two rows have the same properties."""
+
         if other.__class__ is not Row:
             return False
         if self.number != other.number:
             return False
         if len(self.components) != len(other.components):
+            return False
+        if self.ref != other.ref:
             return False
         return reduce(lambda x, y: x and y, [self.components[i] == other.components[i] for i in range(len(self.components))])
--- tests/row_tests.py
+++ tests/row_tests.py
@@ -1,17 +1,20 @@
 import os
+import re
 import sys
 import unittest
 
 sys.path.append(os.path.join('..', 'classes'))
-from pattern import *
-from section import *
-from row import *
 from annotation import *
+from pattern import *
+from row import *
+from section import *
 
 sys.path.append(os.path.join('..', ''))
 import knitparser
 
 class TestIllegalInitialization(unittest.TestCase):
+    """Test row constructor constraints."""
+
     def test_initialize_empty_list(self):
         self.assertRaises(Exception, Row, [], 2)
 
@@ -24,7 +27,31 @@ class TestIllegalInitialization(unittest.TestCase):
     def test_initialize_with_list_of_non_annotations(self):
         self.assertRaises(Exception, Row, ['cat', 'dog'], 2)
 
-class TestRowParsing(unittest.TestCase):
+class TestUnitRowParsing(unittest.TestCase):
+    """Test parsing individual rows outside of the context of a pattern."""
+
+    def test_first_row(self):
+        pattern = 'Row 1. Ooh here is a row!'
+        row = Row([Annotation('Ooh here is a row!')], 1)
+        self.assertEquals(row, knitparser.parse_row(pattern, re.match(knitparser.ROW_REGEX, pattern)))
+
+    def test_nth_row(self):
+        pattern = 'Row 5. Ooh here is a row!'
+        row = Row([Annotation('Ooh here is a row!')], 5)
+        self.assertEquals(row, knitparser.parse_row(pattern, re.match(knitparser.ROW_REGEX, pattern)))
+
+    def test_first_round(self):
+        pattern = 'Round 1. Ooh here is a row!'
+        row = Row([Annotation('Ooh here is a row!')], 1)
+        self.assertEquals(row, knitparser.parse_row(pattern, re.match(knitparser.ROW_REGEX, pattern)))
+
+    def test_nth_round(self):
+        pattern = 'Round 25. Ooh here is a row!'
+        row = Row([Annotation('Ooh here is a row!')], 25)
+        self.assertEquals(row, knitparser.parse_row(pattern, re.match(knitparser.ROW_REGEX, pattern)))
+
+class TestE2ERowParsing(unittest.TestCase):
+    """Test parsing rows within the context of a pattern."""
 
     def setUp(self):
         self.tree = Pattern('Test Pattern', [
@@ -55,13 +82,13 @@ class TestRowParsing(unittest.TestCase):
         ])
         self.assertEqual(tree, knitparser.parse(pattern))
 
-    def test_labelled_rows(self):
+    def test_multiple_rows(self):
         pattern = 'Test Pattern\nBlah blah this is a pattern.\nRow 1. Ooh here is a row!\nRow 2. Wow, another one!'
         self.assertEqual(self.tree, knitparser.parse(pattern))
         pattern = 'Test Pattern\nBlah blah this is a pattern.\nRow 1: Ooh here is a row!\nRow 2: Wow, another one!'
         self.assertEqual(self.tree, knitparser.parse(pattern))
 
-    def test_labelled_rounds(self):
+    def test_multiple_rounds(self):
         pattern = 'Test Pattern\nBlah blah this is a pattern.\nRound 1. Ooh here is a row!\nRound 2. Wow, another one!'
         self.assertEqual(self.tree, knitparser.parse(pattern))
         pattern = 'Test Pattern\nBlah blah this is a pattern.\nRound 1: Ooh here is a row!\nRound 2: Wow, another one!'
