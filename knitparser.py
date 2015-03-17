4f827d8fc868ceff0f63e57b6a395ed92aa320de
Lily Seropian <lilyseropian@gmail.com>
2015-03-17 19:02:13 -0400

    Repeated row parsing works for all test patterns

--- knitparser.py
+++ knitparser.py
@@ -15,13 +15,13 @@ NUMBER_REGEX = '\d+'
 END_REGEX = '[\.:]?'
 ROW_REGEX = re.compile(LABEL_REGEX + NUMBER_REGEX + SIDE_REGEX + END_REGEX)
 REPEAT_REGEX = re.compile('.*[rR]ep\s[rR]ows|[rR]epeat\s[rR]ows')
-EVERY_OTHER_REGEX = re.compile('.*and all[.]{,15}rows')
+EVERY_OTHER_REGEX = re.compile('.*and all.{,15}rows')
 
 def parse(pattern):
     pattern = pattern.splitlines()
     components = []
     rows = {
-        'count': 0
+        'next_row': 1
     }
     title = pattern[0]
     for line in pattern[1:]:
@@ -43,7 +43,7 @@ def parse(pattern):
                         new_row = parse_row(line, match)
                         components.append(new_row)
                         rows[new_row.number] = new_row
-                        rows['count'] += 1
+                        rows['next_row'] = new_row.number + 1
                     else:
                         components.append(Annotation(line))
 
@@ -77,11 +77,11 @@ def parse_repeat(line, match, rows):
         times = num_in_repeat/num_in_ref
         
         if times == 1:
-            parsed_rows = [Reference(rows[i], rows['count'] + i - ref_start + 1) for i in range(ref_start, ref_end + 1)]
-            rows['count'] = repeat_end
+            parsed_rows = [Reference(rows[i], rows['next_row'] + i - ref_start) for i in range(ref_start, ref_end + 1)]
+            rows['next_row'] = repeat_end + 1
             return parsed_rows
         
-        rows['count'] = repeat_end
+        rows['next_row'] = repeat_end + 1
         parsed_rows = [Reference(rows[i]) for i in range(ref_start, ref_end + 1)]
         return Repeat(parsed_rows, repeat_start, num_in_repeat/num_in_ref)
     
@@ -89,15 +89,15 @@ def parse_repeat(line, match, rows):
         ref_start, ref_end = nums_after[0], nums_after[-1]
         times = nums_before[0]/(ref_end - ref_start + 1)
         if times == 1:
-            parsed_rows = [Reference(rows[i], rows['count'] + i - ref_start + 1) for i in range(ref_start, ref_end + 1)]
-            rows['count'] += nums_before[0]
+            parsed_rows = [Reference(rows[i], rows['next_row'] + i - ref_start) for i in range(ref_start, ref_end + 1)]
+            rows['next_row'] += nums_before[0]
             return parsed_rows
 
-        rows['count'] += nums_before[0]
+        rows['next_row'] += nums_before[0]
         repeated_rows = [Reference(rows[i]) for i in range(ref_start, ref_end + 1)]
-        return Repeat(repeated_rows, rows['count'] - nums_before[0] + 1, times)
-    # TODO: figure out other cases
-    return Repeat([Annotation(line)], rows['count'])
+        return Repeat(repeated_rows, rows['next_row'] - nums_before[0], times)
+    rows['next_row'] += 1
+    return Repeat([Annotation(line)], rows['next_row'] - 1)
     
 def parse_repeat_every_other(line, match):
     header, body = line[:line.index(':')], line[line.index(':') + 1 :]
@@ -129,8 +129,8 @@ def find_all_nums(line):
     return nums
 
 if __name__ == '__main__':
-    # pat = open('tests/test_files/scarf-beginner.txt', 'r')
-    pat = open('tests/test_files/scarf-intermediate.txt', 'r')
+    pat = open('tests/test_files/scarf-beginner.txt', 'r')
+    # pat = open('tests/test_files/scarf-intermediate.txt', 'r')
     # pat = open('tests/test_files/scarf-advanced.txt', 'r')
     pat_lines = pat.read()
     pat.close()
