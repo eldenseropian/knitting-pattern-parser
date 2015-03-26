d3a04b50fd12cc359e619feb1661a9a6207451f2
Lily Seropian <lilyseropian@gmail.com>
2015-03-26 13:14:01 -0400

    Added script to run all the tests at once

---
+++ tests/run_all_tests.py
@@ -0,0 +1,18 @@
+import os
+
+print '\n\nRUNNING PATTERN TESTS'
+os.system('python pattern_tests.py')
+
+print '\n\nRUNNING IN-ROW REPEAT TESTS'
+os.system('python in_row_repeat_tests.py')
+
+print '\n\nRUNNING ROW TESTS'
+os.system('python row_tests.py')
+
+print '\n\nRUNNING REPEAT TESTS'
+os.system('python repeat_tests.py')
+
+print '\n\nRUNNING PARSER TESTS'
+os.system('python parser_tests.py')
+
+print '\n'
